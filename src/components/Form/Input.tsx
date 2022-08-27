2773dc5916828257c1e248f2d031463b3699d6b0
DevsJ <douglasmb87@gmail.com>
2022-08-27 18:58:12 -0300

    -add header

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { ReactElement } from 'react';
+
+interface InputProps extends ChakraInputProps {
+    name: string;
+    label?: string;
+}
+
+const Input = ({ name, label, ...rest }: InputProps) => {
+    return (
+        <FormControl>
+            {!!label && <FormLabel htmlFor='email'>{label}</FormLabel>}
+            <ChakraInput
+                name={name}
+                id={name}
+                focusBorderColor='pink.500'
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{
+                    bgColor: "gray.900"
+                }}
+                size="lg"
+                {...rest}
+            />
+
+        </FormControl>
+    )
+}
+
+export default Input; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,29 @@
 import type { NextPage } from 'next'
-import {Flex, Input, Button, Stack } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import Input from '../components/Form/Input';
 
 const Home: NextPage = () => {
   return (
-     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
-        <Flex
-         as="form"
-         width="100%"
-         maxWidth={360}
-         bg="gray.800"
-         p="8"
-         borderRadius={8}
-         flexDir="column"
-         >
-            <Stack spacing="4">
-          <Input 
-          name="email" 
-          type="email" 
-          focusBorderColor='pink.500'
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor:"gray.900"
-          }}
-          size="lg"
-          />
-          
-          <Input 
-          name="password" 
-          type="password"
-          focusBorderColor='pink.500'
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor:"gray.900"
-          }}
-          size="lg"
-          />
-      </Stack>
-          <Button type="submit" mt="6" size="large" colorScheme={'pink'}>Entrar</Button>
-        </Flex>
-     </Flex>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name='email' label='E-mail' type="email" />
+          <Input name='password' label='Senha' type="password" />
+        </Stack>
+        <Button
+          type="submit"
+          mt="6" size="lg"
+          colorScheme={'pink'}>Entrar</Button>
+      </Flex>
+    </Flex>
   )
-  }
+}
 export default Home;
