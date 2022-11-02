3234e49fd8d1f5d1077635866f25a267b6f32aeb
Douglas Sousa <douglasmb87@gmail.com>
2022-11-01 22:11:35 -0300

    (refactor: layout resposive )

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Douglas Sousa</Text>
-        <Text color="gray.300" fontSize="small">
-          Douglasmb87@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Douglas Sousa</Text>
+          <Text color="gray.300" fontSize="small">
+            Douglasmb87@gmail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar
         size="md"
         name="Douglas Sousa"
